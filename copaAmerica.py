47ea5a08a2c5d32f5e1fd403b21d7d6958802020
Jeferson Jimenez <jeferson.jl99@gmail.com>
2021-06-28 12:34:59 -0500

    agregamos copaAmerica

---
+++ copaAmerica.py
@@ -0,0 +1,20 @@
+out = ""
+s=""
+out1 = ""
+n = " "
+cont = 0
+s = input()
+
+s=s.replace(" ", "")
+n=s[0]
+for i in s:
+    if i == n:
+        cont += 1
+    else:
+        out += str(cont)+" "
+        out1 += n+" "
+        cont = 1
+        n = i
+out += str(cont)+" "
+out1 += n+" "
+print(out1+"\n"+out)
